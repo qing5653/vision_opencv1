22c5b4f808f04167ad220d93113d97fb0fc67454
Elaina <1463967532@qq.com>
2025-04-07 03:28:34 +0000

    feat (cv_bridge)创建了返回图片,但是zmq有问题

--- cv_lib/cv_bridge.py
+++ cv_lib/cv_bridge.py
@@ -3,6 +3,7 @@ import zmq
 import multiprocessing.shared_memory as shm
 import numpy as np
 import time
+import os
 class ImagePublish_t:
     """
         将opencv图像发布到ROS 2话题
@@ -10,26 +11,47 @@ class ImagePublish_t:
         :param topic: 话题名称
         :param queue_size: 消息队列大小
     """
-    def __init__(self,node, topic:str,queue_size:int=10):
-        self._node = node
+    def __init__(self, topic:str,socket="tcp://localhost:5556"):
         self._topic = topic
-        self._queue_size = queue_size
         self._image_publish={}
-        # self._publisher=self._node.create_publisher(Image, self._topic, self._queue_size)
-        # self._copy = copy
-    def update(self, image:cv2.Mat,content:dict):
+        self.shm_name=None
+        self.shm=None
+        self.shm_size=None
+        #zmq初始化
+        ctx=zmq.Context()
+        self.socket=ctx.socket(zmq.PUSH)
+        self.socket.bind(socket)  # 绑定到指定地址
+    def update(self, image:np.ndarray,content:dict=None):
         # self.node.get_logger().info(f"Publishing image to {self.topic}")
         """
-        将 OpenCV 图像发布到 ROS 2 话题
+        将 OpenCV 图像通过共享内存发布到 ROS 2 话题
         :param image: OpenCV 图像对象 (np.ndarray)
         :param content: 附加的内容，如时间戳、坐标系等
         """
-        # ros_image = self._bridge.cv2_to_imgmsg(image, encoding="bgr8")
-        #默认复制传入图像的header
-        # ros_image.header=content['header']
-        # # 发布图像消息
-        # self._publisher.publish(ros_image)
-
+        image_shape=    image.shape
+        image_size=image.nbytes
+        # 创建共享内存
+        if self.shm_name==None or self.shm_size!=image_size or not os.path.exists(f"/dev/shm/{self.shm_name}"):
+            if self.shm:
+                try:
+                    self.shm.close()
+                    self.shm.unlink()
+                except Exception as e:
+                    print("Error closing shared memory:", e)
+            self.shm=shm.SharedMemory(create=True, size=image_size)
+            self.shm_name=self.shm.name
+            self.shm_size=image_size
+            print(f"创建共享内存{self.shm_name}，大小{self.shm_size}字节")
+        # 将图像数据复制到共享内存
+        np_shm_buf = np.ndarray(image_shape, dtype=image.dtype, buffer=self.shm.buf)
+        np.copyto(np_shm_buf, image)
+        self.socket.send_json({
+            'shm_key': self.shm_name,
+            'shape': image_shape,
+            'dtype': str(image.dtype),
+            'timestamp': time.time(),
+            'topic': self._topic,
+        })
 class ImageReceive_t:
     """
     将共享内存图像转化成cv2图像
@@ -37,9 +59,9 @@ class ImageReceive_t:
     """
     def __init__(self, socket="tcp://localhost:5555",print_latency=False,im_show=False):
         self._context = zmq.Context()
-        self._socket = self._context.socket(zmq.SUB)
+        self._socket = self._context.socket(zmq.PULL)
         self._socket.connect(socket)  # 连接到指定地址
-        self._socket.setsockopt_string(zmq.SUBSCRIBE, "")  # 订阅所有消息（这一行是关键）
+        # self._socket.setsockopt_string(zmq.SUBSCRIBE, "")  # 订阅所有消息（这一行是关键）
         self.shm=None
         self.shm_key=None
         self.im_show=im_show
@@ -92,10 +114,15 @@ class ImageReceive_t:
             
 def main():
     receive = ImageReceive_t(print_latency=True,im_show=True)
+    pub1= ImagePublish_t("test_topic", socket="tcp://localhost:5556")
+    pub2= ImagePublish_t("test_topic2")
     while True:
         # 创建一个空的图像对象，这里用一个全黑的图像作为示例
         image = np.zeros((1, 1, 3), dtype=np.uint8)  # 假设图像大小为480x640，3通道（RGB）
         receive.update(image)  # 更新图像为共享内存中的内容
+        # 发布图像
+        pub1.update(image)
+        pub2.update(image)
         
 if __name__ == "__main__":
     main()
