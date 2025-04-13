226de9c0775ddbf44f7336441ee1e0dcc7457d8b
Elaina <1463967532@qq.com>
2025-04-13 14:51:41 +0000

    feat(docker)增加了一个ros2 的dockerfile不一定会维护

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -4,6 +4,10 @@
 if [ "$1" == "--cpu" ]; then
     # 如果是 --cpu 参数，使用 cpu.dockerfile
     docker build -t elaina/yolo_image -f cpu.dockerfile .
+#如果传入ros
+elif [ "$1" == "--ros" ]; then
+    # 如果是 --ros 参数，使用 ros.dockerfile
+    docker build -t elaina/yolo_image -f ros.dockerfile .
 else
     # 默认情况，使用 Dockerfile（假设该文件是默认的）
     docker build -t elaina/yolo_image .
